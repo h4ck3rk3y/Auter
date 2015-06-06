eb7559b9bdb86d94c62e8fd16467ffe154938515
h4ck3rk3y <anomaly.the@gmail.com>
2015-06-06 19:28:46 +0530

    implemented login succesuflly

--- app/src/main/java/com/zephrys/auter/MainActivity.java
+++ app/src/main/java/com/zephrys/auter/MainActivity.java
@@ -1,43 +1,39 @@
 package com.zephrys.auter;
 
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.conn.ssl.X509HostnameVerifier;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.SingleClientConnManager;
 import org.apache.http.message.BasicHeader;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.protocol.HTTP;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
+
+import javax.net.ssl.HostnameVerifier;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -46,48 +42,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
 
-        Button b1 = (Button) findViewById(R.id.signup);
 
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //get the various required fields
-                String aadhar = (String) ((TextView) findViewById(R.id.aadhar)).getText();
-                String first_name = (String) ((TextView) findViewById(R.id.fname)).getText();
-//                String last_name = (String) ((TextView) findViewById(R.id.lname)).getText();
-                String gender = (String) ((TextView) findViewById(R.id.gender)).getText();
-                String dob = (String) ((TextView) findViewById(R.id.dob)).getText();
-
-                String son1 = String.format("{ \"aadhaar-id\": \"%s\", \"device-id\": \"Zephyr\", \"modality\": \"demo\", \"certificate-type\": \"preprod\", \"demographics\": { \"name\": { \"matching-strategy\": \"P\",\n      \"matching-value\": \"56\",\"name-value\": \"%s\" } , \"dob\": {\n      \"format\": \"YYYY-MM-DD\",\n      \"dob-type\": \"V\",\n      \"dob-value\": \"%s\"\n    }, \"gender\" : \"%s\"}, \"location\": { \"type\": \"gps\", \"latitude\": \"22.3\", \"longitude\": \"73.2\", \"altitude\": \"0\" } }", aadhar, first_name, dob, gender);
-//                Calendar c = Calendar.getInstance();
-//                SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
-//                String timeString = f.format(c.getTime());
-
-                HttpClient client = new DefaultHttpClient();
-                HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
-                HttpResponse response;
-//                JSONObject json = new JSONObject();
-
-                try {
-                    HttpPost post = new HttpPost("https://ac.khoslalabs.com/hackgate/hackathon/auth/raw HTTP/1.1");
-                    StringEntity se = new StringEntity(son1);
-                    se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
-                    post.setEntity(se);
-                    response = client.execute(post);
-
-                    /*Checking response */
-                    if(response!=null){
-                        InputStream in = response.getEntity().getContent(); //Get the data in the entity
-                    }
-
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-
-
-            }
-        });
         super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -122,7 +80,7 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public class PlaceholderFragment extends Fragment {
 
         public PlaceholderFragment() {
         }
@@ -131,6 +89,72 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+            Button b1 = (Button) findViewById(R.id.sign);
+
+            b1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d("I am here", "YE");
+                    //get the various required fields
+                    String aadhar = ((EditText)findViewById(R.id.aadhar)).getText().toString();
+                    TextView t1 = (TextView) findViewById(R.id.op);
+                    String first_name = (String) ((EditText) findViewById(R.id.fname)).getText().toString();
+                    String gender = (String) ((EditText) findViewById(R.id.gender)).getText().toString();
+                    String dob = (String) ((EditText) findViewById(R.id.dob)).getText().toString();
+                    //chages to true if logged in succesfully
+                    Boolean details_correct = false;
+                    // basic json handles name, age, gender. name matching is fuzzy ;)
+                    String son1 = "{ \"aadhaar-id\": \""+aadhar+"\", \"device-id\": \"192.168.0.101\", \"modality\": \"demo\", \"certificate-type\": \"preprod\", \"demographics\": { \"name\": { \"matching-strategy\": \"P\",\"matching-value\": \"60\", \"name-value\": \""+first_name+"\" },\"gender\": \""+gender+"\", \"age\": \""+dob+"\"}, \"location\": { \"type\": \"gps\", \"latitude\": \"22.3\", \"longitude\": \"73.2\", \"altitude\": \"0\" } }";
+                    //a default http client (request object form python)
+                    DefaultHttpClient client = new DefaultHttpClient();
+                    HostnameVerifier hostnameVerifier = org.apache.http.conn.ssl.SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
+
+                    //adding ssl capabilities
+                    SchemeRegistry registry = new SchemeRegistry();
+                    SSLSocketFactory socketFactory = SSLSocketFactory.getSocketFactory();
+                    socketFactory.setHostnameVerifier((X509HostnameVerifier) hostnameVerifier);
+                    registry.register(new Scheme("https", socketFactory, 443));
+                    SingleClientConnManager mgr = new SingleClientConnManager(client.getParams(), registry);
+
+                    DefaultHttpClient httpClient = new DefaultHttpClient(mgr, client.getParams());
+
+
+                    HttpConnectionParams.setConnectionTimeout(httpClient.getParams(), 10000); //Timeout Limit
+
+
+
+                    HttpResponse response;
+                    //JSONObject json = new JSONObject();
+//                    Log.d("I am here", "YE");
+                    try {
+                        HttpPost post = new HttpPost("https://ac.khoslalabs.com/hackgate/hackathon/auth/raw");
+                        StringEntity se = new StringEntity(son1);
+                        se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                        post.setEntity(se);
+                        response = httpClient.execute(post);
+
+                    /*Checking response */
+                        if(response!=null){
+                            Log.d("RESPONSE", "FOUnD");
+                            ResponseHandler<String> handler = new BasicResponseHandler();
+                            String body = handler.handleResponse(response);
+                            Log.d("stream", body);
+                            if(body.indexOf("success\":true") > 0) {
+                                details_correct = true;
+                            }
+                            t1.setText("Details are correct");
+                        }
+
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+            });
+
             return rootView;
         }
     }
